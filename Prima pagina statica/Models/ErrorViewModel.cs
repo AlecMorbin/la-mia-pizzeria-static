7e358afa278a87667687116b47a26d6fc55d18cf
Alec <alemor98@hotmail.it>
2022-05-06 15:41:09 +0200

    Add project files.

---
+++ Prima pagina statica/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Prima_pagina_statica.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
